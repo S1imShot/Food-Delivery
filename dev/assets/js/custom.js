7e779bf0bf9efff39fe802c32f0493be8ea6fd0c
S1imShot <slimshot123@gmail.com>
2024-02-06 20:24:10 +0200

    end first screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,26 @@
-  
+const burger = document.querySelector(".hamburger");
+
+burger.addEventListener("click", function () {
+	burger.classList.toggle("is-active");
+
+});  
+
+var swiper = new Swiper(".mySwiper", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	
+	coverflowEffect: {
+	  rotate: 50,
+	  stretch: 0,
+	  depth: 100,
+	  modifier: 1,
+	  slideShadows: false,
+
+	},
+
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+});
